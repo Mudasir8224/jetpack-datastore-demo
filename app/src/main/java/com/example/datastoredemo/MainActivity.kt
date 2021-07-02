6dbab0366b9aa43de663e2579932e442aa95e819
mudasirBhatti <mohammad.mudasir@transdata.biz>
2021-07-02 11:53:10 +0500

    implement live data

--- app/src/main/java/com/example/datastoredemo/MainActivity.kt
+++ app/src/main/java/com/example/datastoredemo/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.datastoredemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myPref: MyPreference
+    private var button:Button? = null
+    private var et:EditText? = null
+    private var tv:TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        saveNotes()
     }
+
+    private fun initViews(){
+        myPref = MyPreference(this)
+        et = findViewById(R.id.et);
+        tv = findViewById(R.id.tv);
+        button = findViewById(R.id.button);
+    }
+
+    private fun saveNotes(){
+        button?.setOnClickListener {
+            val notes = et?.text.toString().trim()
+            lifecycleScope.launch { myPref
+                .saveNotes(notes)}
+        }
+
+        myPref.notes.asLiveData().observe(this, Observer {
+            tv?.text = it
+        })
+
+    }
+
 }
---
+++ app/src/main/java/com/example/datastoredemo/MyPreference.kt
@@ -0,0 +1,37 @@
+package com.example.datastoredemo
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class MyPreference(context:Context) {
+
+    private val applicationContext = context.applicationContext
+    private val dataStore: DataStore<Preferences>
+
+    init {
+        dataStore = applicationContext.createDataStore(name = "app_preferences")
+    }
+
+    val notes: Flow<String?>
+        get() = dataStore.data.map { preferences ->
+            preferences[KEY_NOTES]
+        }
+
+    suspend fun saveNotes(notes:String){
+        dataStore.edit { preferences ->
+            preferences[KEY_NOTES] = notes
+        }
+    }
+
+
+    companion object {
+        val KEY_NOTES = preferencesKey<String>("key_notes")
+    }
+
+}
